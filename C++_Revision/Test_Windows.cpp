0b998f1c80981a06aa7899bbd41cb17f44add385
hazem <hazemkhaled3443@gmail.com>
2022-11-19 11:50:14 +0200

    running C++ from vscode tasks.json configured

---
+++ C++_Revision/Test_Windows.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    for (int i:myNumbers) {
+        cout << i << "\n";
+    }
+}
